c604bd80695234f1160f146665fa7c847e4c18f8
anturashova <30anna30.08@gmail.com>
2022-03-11 23:34:24 +0000

    Add urls and API DELETE

--- post_in/api/views.py
+++ post_in/api/views.py
@@ -4,6 +4,27 @@ from rest_framework import status
 from notes.models import Note
 from api.serializers import NoteSerializer
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def notes_detail(request, pk, format=None):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET', 'POST'])
 def notes_list(request, format=None):
     if request.method == 'GET':
